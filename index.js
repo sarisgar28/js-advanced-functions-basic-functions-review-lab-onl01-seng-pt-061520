466104aea34107d4a5a351b50cb7baaee2b7885e
sarisgar28 <sarisgar28@gmail.com>
2021-01-12 15:50:22 -0500

    commit tree 7f5818e4d9eac16523cc06e30f740c06dabec9d6

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-// Your code here
-function saturdayFun(fun= "This Saturday, I want to roller-skate!"){
-console.log(fun);
+// Your code here)
+function saturdayFun(activity="roller-skate" ){
+  return (`This Saturday, I want to ${activity}!`);
+};
+
+
+function mondayWork(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(highlight="*"){
+ return function(adjective){
+    return (`You are ${highlight}${adjective}${highlight}!`)
+}   
+}
+
+const Calculator = {
+    add: function addCalculator(y,x){
+        return y + x 
+    },
+    subtract: function subtractCalculator(y,x){
+        return y - x
+    },
+
+    multiply: function multiplyCal(y,x){
+        return y * x
+    },
+    divide: function divideCal(y,x){
+        return y / x 
+    },
+}
+
+function actionApplyer(i,arr){
+  arr.forEach(fn => {
+      i = fn(i)
+  });
+  return i 
 }
